7c7da82dbdc651b562b06ebb4ac3e1e9c5ece2d3
Dr Aha <drahadataai@gmail.com>
2025-05-31 21:14:36 +1000

    remove key

--- src/utils/model_clients.py
+++ src/utils/model_clients.py
@@ -2,7 +2,7 @@ from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 openai_model_client = OpenAIChatCompletionClient(
     model="gpt-4o",
-    api_key="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",
+    api_key="#########",
     max_retries=3,
     retry_delay=2,
     timeout=30,
@@ -11,7 +11,7 @@ openai_model_client = OpenAIChatCompletionClient(
 
 openai_model_client_o4_mini = OpenAIChatCompletionClient(
     model="o4-mini",
-    api_key="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",
+    api_key="########",
     max_retries=3,
     retry_delay=2,
     timeout=30,
